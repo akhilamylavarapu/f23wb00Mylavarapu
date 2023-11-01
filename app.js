f7d075f5821dbb3599a4b8db56980704044a20ff
AkhilaMylavarapu <akhila.smr23@gmail.com>
2023-11-01 00:49:22 -0500

    added new endpoint choose

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var jacketsRouter = require('./routes/jackets');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/jackets', jacketsRouter);
 app.use('/board', boardRouter);
+app.use('/choose',chooseRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
